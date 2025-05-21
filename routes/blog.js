99df209b0bb98757067c288463ef6ffde1449045
divyesh1111 <divyeshhirpara000@gmail.com>
2025-05-21 14:52:28 -0700

    commit 3: add the slug

--- routes/blog.js
+++ routes/blog.js
@@ -58,4 +58,16 @@ router.delete('/blogs/:id', authMiddleware, async (req, res) => {
   }
 });
 
+// GET /blogs/:id
+router.get("/blogs/:id", async (req, res) => {
+  try {
+    const blog = await Blog.findById(req.params.id);
+    if (!blog) return res.status(404).json({ message: "Blog not found" });
+    res.json(blog);
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+
 module.exports = router;
